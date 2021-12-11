e1ebd22307de53fcc5005e49c00e61f11db952b0
PhilPinsdorf <philemon.pinsdorf@live.de>
2021-12-11 11:55:57 +0100

    Initial Commit without Netlify

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const router = require('./routes/router.js');
+
+const mongoDbUri = 'mongodb+srv://expressapp:C4xqzz4YCQkzF2L@ezpp.yyk9j.mongodb.net/EzPP?retryWrites=true&w=majority'
+
+//Express Instance
+const app = express();
+app
+.use(express.static('public'))
+.use(cors())
+.use(cookieParser())
+.use(express.json())
+.use(express.urlencoded({extended: true}))
+.use('/', router);
+
+//Database Connection and opening of Port
+mongoose
+	.connect(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => {
+		console.log('Connected to Database');
+		app.listen(3000, () => console.log('Listen to 3000'));
+	})
+	.catch((err) => {
+		console.log(err);
+	});
