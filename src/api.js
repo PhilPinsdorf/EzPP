df66f7e148c135e3b6449c2a5c4f2c5a9e53e6aa
PhilPinsdorf <philemon.pinsdorf@live.de>
2022-01-07 22:29:41 +0100

    Moved router to api

--- src/api.js
+++ src/api.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const serverless = require('serverless-http')
+const express = require('express');
 const randomString = require('./utils/randomString.js');
 const querystring = require('querystring');
 const request = require('request')
 const path = require('path');
 const User = require('./modules/user.js');
 const base64 = require('base-64');
-const { urlencoded } = require('express');
 
+const mongoDbUri = 'mongodb+srv://expressapp:C4xqzz4YCQkzF2L@ezpp.yyk9j.mongodb.net/EzPP?retryWrites=true&w=majority'
+
+//Express Instance
+const app = express();
 const router = express.Router();
+app.use(express.static('public'))
+app.use('/.netlify/functions/api', router);
 
 var client_id = '7c4553c111d241b7ba3f7038f77e2e87'; 
 var client_secret = '5bbe8d46b303428b993a475250e31278'; 
@@ -374,5 +384,19 @@ router.get('/api/v1/getenabled', (req, res) => {
 	});
 })
 
-module.exports = router;
-exports.handler = router;
+//Database Connection and opening of Port
+mongoose
+	.connect(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => {
+		console.log('Connected to Database');
+		app.listen(3000, () => console.log('Listen to 3000'));
+		app.use(cors())
+		app.use(cookieParser())
+		app.use(express.json())
+		app.use(express.urlencoded({extended: true}))
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+module.exports.handler = serverless(app)
