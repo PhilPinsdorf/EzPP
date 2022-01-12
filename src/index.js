020a83749e0c9799de0e136522f5a9e342efaf3c
PhilPinsdorf <philemon.pinsdorf@live.de>
2022-01-12 22:22:56 +0100

    Possible Fix?

--- src/index.js
+++ src/index.js
@@ -16,19 +16,19 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', routing);
-app.use('/api/v1/', api);
-app.use('/fileshare/', fileshare);
 
-// Limit Request per IP
+// Limit Request per IP To prevent DDoS
 const limiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 15 minutes
+	windowMs: 1 * 60 * 1000, // 1 minutes
 	max: 50, // Limit each IP to 50 requests per `window` (here, per 1 minute)
 	message: 'Too many Requests', // Received Message
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 app.use(limiter);
+app.use('/', routing);
+app.use('/api/v1/', api);
+app.use('/fileshare/', fileshare);
 
 //Database Connection and opening of Port
 mongoose
